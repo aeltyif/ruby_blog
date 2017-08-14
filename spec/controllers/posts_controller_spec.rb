6c73c9cfad054a8ebedfe21230c663aa19d35619
aeltyif <a.albueltyif@boundlessdrop.com>
2017-08-14 16:49:46 +0300

    RSpec works now, HELL YEAH

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    sign_in(user)
+  end
+
   describe "GET #index" do
     it "show the list of posts" do
       get :index
@@ -9,15 +14,13 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe 'POST #create' do
-    let(:user) { FactoryGirl.create(:user, email: 'grill@bd.com', password: '123456', password_confirmation: '123456', name: 'Grill') }
-
     it "returns http created" do
       params = {
         title: 'Grill Title',
         body: 'Hello this is a testing body here'
       }
       post :create, params: { :post => params }
-      expect(response).to have_http_status(:created)
+      expect(response).to have_http_status(302)
     end
   end
 end
