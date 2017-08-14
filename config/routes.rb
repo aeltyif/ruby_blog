1200af7b7492c38dbf3dbffb0bde7561807bf5b5
aeltyif <a.albueltyif@boundlessdrop.com>
2017-08-14 11:53:29 +0300

    modified the registration by adding the name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: redirect('/posts')
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
 end
